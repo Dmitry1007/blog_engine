92a4296fb51b70d88b7e254f9b6fe6008868e635
Dmitry Sky <dmitry.sky@icloud.com>
2015-05-07 11:22:30 -0600

    added second article inception for testing

--- articles/inception.md
+++ articles/inception.md
@@ -1,6 +1,6 @@
 title: Updated
-date: 2015-05-06
+date: 2015-03-7
 
 Hello friends! Sorry, I haven't blogged in quite a while. I was busy reading
-[a book](http://oreilly.com/catalog/0636920019664/) to improve my Sinatra
+[a book](http://www.google.com) to improve my Sinatra
 skills. I will blog more from now on, I promise.
--- lib/blog.rb
+++ lib/blog.rb
@@ -1,24 +1,23 @@
 require 'sinatra/base'
-require 'github_hook'
+# require 'github_hook'
 require 'ostruct'
 require 'time'
 require 'yaml'
 
 class Blog < Sinatra::Base
-  use GithubHook
+  # use GithubHook
 
   set :root, File.expand_path('../../', __FILE__)
+  # File.expand_path('../../, __FILE__) = "/Users/Dmitry/sinatra"
+  # root = "/Users/Dmitry/sinatra/blog_engine"
+  
   set :articles, []
-  set :app_file, __FILE__
-
-  # File.expand_path generates an absolute path.
-  # It also takes a path as second argument. 
-  # The generated path is treated as being relative to that path.
-  set :root, File.expand_path('../../', __FILE__)
-  set :articles, []
+  set :app_file, __FILE__ # => "lib/blog.rb"
 
   # loop through all the article files
-  Dir.glob "#{root}/articles/*.md" do |file|
+  Dir.glob "#{root}/articles/*" do |file|
+    # file = "/Users/Dmitry/sinatra/blog_engine/articles/updated.md"
+    
     # parse meta data and content from file
     meta, content   = File.read(file).split("\n\n", 2)
     # generate a metadata object
@@ -27,12 +26,16 @@ class Blog < Sinatra::Base
     article.date    = Time.parse article.date.to_s
     # add the content
     article.content = content
-    # generate a slug for the url
+    
     article.slug    = File.basename(file, '.md')
-    # set up the route
+    # = "updated"
+
     get "/#{article.slug}" do
       erb :post, :locals => { :article => article }
     end
+    # = [/\A\/updated\z/, [], [],
+    #<Proc:0x007ff03b262e28@/Users/Dmitry/.rvm/gems/ruby-2.1.5/gems/sinatra-1.4.6/lib/sinatra/base.rb:1610>]
+
     # Add article to list of articles
     articles << article
   end
@@ -40,7 +43,12 @@ class Blog < Sinatra::Base
   articles.sort_by! { |article| article.date }
   articles.reverse!
 
+  # defining and ERB template using inline approach
   get '/' do
     erb :index
   end
+  # = [/\A\/\z/, [], [],
+  #<Proc:0x007f9c6a260508@/Users/Dmitry/.rvm/gems/ruby-2.1.5/gems/sinatra-1.4.6/lib/sinatra/base.rb:1610>]
 end
+# \A = Start of string, \z = End of string
+
